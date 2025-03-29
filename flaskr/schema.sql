69beaf404083bc36062614b7e56253b2395d0259
Lucas-Weaver <lucasweavera@gmail.com>
2025-03-29 13:34:17 -0400

    initial commit

---
+++ flaskr/schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS meals;
+
+CREATE TABLE meals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    meal_name TEXT UNIQUE NOT NULL,
+    meal_description TEXT UNIQUE NOT NULL,
+    
+    calories INTEGER NOT NULL,
+    protein INTEGER NOT NULL,
+    sodium INTEGER NOT NULL,
+    suger INTEGER NOT NULL,
+    fat INTEGER NOT NULL,
+    fiber INTEGER NOT NULL,
+    carbohydrates INTEGER NOT NULL,
+
+    vegan BOOLEAN NOT NULL,
+    vegetarian BOOLEAN NOT NULL,
+    gluten_free BOOLEAN NOT NULL,
+
+
+    
+    meal_time TEXT NOT NULL,
+    dining_hall TEXT NOT NULL,
+    restaurant TEXT NOT NULL,
+
+    eggs BOOLEAN,
+    fish BOOLEAN,
+    milk BOOLEAN,
+    peanuts BOOLEAN,
+    sesame BOOLEAN,
+    shellfish BOOLEAN,
+    soy BOOLEAN,
+    treenuts BOOLEAN,
+    wheat BOOLEAN
+
+
+
+
+);
